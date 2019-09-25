662c8cacb38d62373dcb429db0c2635d03ca8a51
snyder <snyderjosephd@yahoo.com>
2019-09-25 12:09:37 -0500

    Build Board component

---
+++ src/components/FallenSoldiers.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Square from './Square';
+import '../index.css';
+
+class FallenSoldiers extends Component {
+
+    renderSquare(square, i, squareShade) {
+        return <Square 
+            piece={square}
+            style={square.style}
+        />
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">{this.props.whiteFallenSoldiers.map((ws, index) =>
+                    this.renderSquare(ws, index)
+                )}
+                </div>
+                <div className="board-row">{this.props.blackFallenSoldiers.map((bs, index) =>
+                    this.renderSquare(bs, index)    
+                )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FallenSoldiers;
